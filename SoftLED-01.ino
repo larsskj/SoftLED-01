b44e6f100911da7d1d5cbb8c8824088dd8efb877
Lars Skjærlund <lskjaerlund@liveintent.com>
2020-05-23 17:47:51 +0200

    Sketchbook now with comments

--- SoftLED-01.ino
+++ SoftLED-01.ino
@@ -1,6 +1,50 @@
+/*
+ * SoftLED-01
+ * 
+ * This small program is primarily meant for use in Danish model railroading.
+ * 
+ * It's intended to be used for a level crossing with flashing lights and 
+ * provides soft turn on/off for LEDs.
+ * 
+ * It has a single input that switches between on and off state. This pin can 
+ * be changed using a mechanical switch or something like a DCC accessory 
+ * controller.
+ * 
+ * When off, the unorderPins are switched on to simulate a steady light in
+ * the "Unorder signal" as it is called in translated Danish.
+ * 
+ * When on, the unorderPins are switched off and the warningsPins will 
+ * provide a flashing light for red warning lights next to the road.
+ * 
+ * The circuit uses PWM outputs for controlling the LEDs. A standard
+ * Arduino has six pins that can be used for PWM - 3, 5, 6, 9, 10, and 11. 
+ * Each pin has a maximum current of 40 mA, so your LEDs cannot draw more
+ * current on a single port.
+ * 
+ * Likewise, the Arduino has an overall maximum of 200 mA for all ports, 
+ * so you cannot draw more total current on all ports combined.
+ * 
+ * LEDs should be connected to a positive voltage and will be connected 
+ * to ground through the output pins.
+ * 
+ * Created May 2020 by Lars Skjærlund <lars@skjaerlund.dk>
+ * 
+ * The code is placed in the public domain.
+ * 
+ */
+
+
+/*
+ * User configurable pin settings. Use whatever fits your Arduino.
+ */
+byte inputPin = 2;
 byte unorderPins[] = {10, 11};
 byte warningPins[] = {3, 5, 6, 9};
 
+
+/*
+ * Timing values for turning LEDs on/off and flash length.
+ */
 unsigned int ledDn = 20;
 unsigned int ledHold = 250;
 unsigned int ledUp = 80;
@@ -8,6 +52,10 @@ unsigned int ledUp = 80;
 unsigned int unorderDn = 40;
 unsigned int unorderUp = 100;
 
+
+/*
+ * The program without any real user settings.
+ */
 const byte STATE_UNORDER = 0;
 const byte STATE_WARNING = 1;
 const byte STATE_UN2WARN = 2;
@@ -20,6 +68,9 @@ bool inFlash = false;
 unsigned int ledStart;
 
 
+/*
+ * Write to all unorderPins.
+ */
 void unorderWrite(byte value) {
   for (int i = 0; i < sizeof(unorderPins); i++) {
     analogWrite(unorderPins[i], value);
@@ -27,6 +78,9 @@ void unorderWrite(byte value) {
 }
 
 
+/*
+ * Write to all warningPins.
+ */
 void warningWrite(byte value) {
   for (int i = 0; i < sizeof(warningPins); i++) {
     analogWrite(warningPins[i], value);
@@ -34,6 +88,9 @@ void warningWrite(byte value) {
 }
 
 
+/*
+ * Handle LED outputs according to time and state.
+ */
 void ledHandler() {
   unsigned int delta = millis() - ledStart;
   int i;
@@ -80,8 +137,11 @@ void ledHandler() {
 }
 
 
+/*
+ * Get desired state by reading the inputPin.
+ */
 byte getState() {
-  if (digitalRead(2)) {
+  if (digitalRead(inputPin)) {
     return STATE_WARNING;
   } else {
     return STATE_UNORDER;
@@ -89,14 +149,20 @@ byte getState() {
 }
 
 
+/*
+ * Standard Arduino setup() routine.
+ */
 void setup() {
-  pinMode(2, INPUT);
+  pinMode(inputPin, INPUT);
   
   currentState = getState();
   desiredState = getState();
 }
 
 
+/*
+ * Standard Arduino loop() for running the program.
+ */
 void loop() {
   desiredState = getState();
   
